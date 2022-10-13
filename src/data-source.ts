90023aa34ae16ce4f3a2d814ce44e392bfb8e316
mehdi <parastar.mehdi@gmail.com>
2022-10-13 13:37:05 +0330

    pre production commit (updated)
    "dev_env": {
          "initial_migration": {
            "step1": "make sure that the `devDB.sqlite` file and `migration_dev` folder don`t exist.",
            "step2": "run the command `npm run migration:dev:generate -name=MigrationName`.",
            "step3": "run the command `npm run migration:dev:run`.",
            "step4": "run the command `npm run prebuild`.",
            "step5": "run the command `npm run build`.",
            "step6": "start the application with the `npm run start:dev`."
          },
          "next_migrations": {
            "step1": "run the command `npm run migration:dev:generate -name=MigrationName`.",
            "step2": "run the command `npm run migration:dev:run`."
          }
        },
        "prod_env": {
          "initial_migration": {
            "step1-1": "make sure that postgres db has been installed and initiated and `migration_prod` folder don`t exist.",
            "step1-2": "for installing and initating postgres you could run the command: `docker run --name postgres -d --rm -e POSTGRES_USER=root -e POSTGRES_PASSWORD=mehdi -p 5432:5432 postgres -c ssl=on -c ssl_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem -c ssl_key_file=/etc/ssl/private/ssl-cert-snakeoil.key`",
            "step2": "run the command `npm run migration:prod:generate -name=MigrationName`.",
            "step3": "run the command `npm run migration:prod:run`.",
            "step4": "run the command `npm run prebuild`.",
            "step5": "run the command `npm run build`.",
            "step6": "start the application with the `npm run start:prod`."
          },
          "next_migrations": {
            "step1": "run the command `npm run migration:prod:generate -name=MigrationName`.",
            "step2": "run the command `npm run migration:prod:run`."
          }
        },
        "migration_reffer_link1": "https://qiita.com/t-yama-3/items/fa2113fcabf84e0a20e6",
        "migration_reffer_link2": "https://qiita.com/Aurum64/items/f5962bd2a643447dbef9",
        "docker_postgres": "docker run --name postgres -d --rm -e POSTGRES_USER=root -e POSTGRES_PASSWORD=mehdi -p 5432:5432 postgres -c ssl=on -c ssl_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem -c ssl_key_file=/etc/ssl/private/ssl-cert-snakeoil.key",
        "heroku_cli_setup_instruction_link1":"https://devcenter.heroku.com/articles/getting-started-with-nodejs#set-up",
        "heroku_cli_setup_instruction_link2":"https://devcenter.heroku.com/articles/heroku-cli",
        "heroku_cli_setup_instruction_link3":"https://www.linuxfordevices.com/tutorials/ubuntu/install-heroku-on-ubuntu"

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,29 +8,43 @@ const dbConfig = {
     database: 'devDB.sqlite',
     entities: [User, Report],
     synchronize: false,
-    migrations: ['src/migration/*.js'],
+    migrations: ['src/migration_dev/*.js'],
   } as DataSourceOptions,
   test: {
     type: 'sqlite',
     database: 'testDB.sqlite',
     entities: [User, Report],
     synchronize: false,
-    migrations: ['src/migration/*.js'],
+    migrations: ['src/migration_dev/*.js'], // Because the DB type at both the `test` and `dev` environments were equal, we used the same directory for migration.
     migrationsRun: true,
   } as DataSourceOptions,
   production: {
+    // ****** -if you want to have production on docker use this configs.
+    // ****** -docker command to create postgres on local docker: `docker run --name postgres -d --rm -e POSTGRES_USER=root -e POSTGRES_PASSWORD=mehdi -p 5432:5432 postgres -c ssl=on -c ssl_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem -c ssl_key_file=/etc/ssl/private/ssl-cert-snakeoil.key`
+    
+    // type: 'postgres',
+    // host: 'localhost',
+    // port: 5432,
+    // migrations: ['src/migration_prod/*.js'],
+    // migrationsRun: true,
+    // entities: [User, Report],
+    // ssl: {
+    //   rejectUnauthorized: false,
+    // },
+    // username: 'root',
+    // password: 'mehdi',
+
+    // ****** -for deploying on heroku use this configs.
+    // ****** -We are going to use the Heroku CLI to deploy our application.
+    // ****** -Please follow the instructions here https://devcenter.heroku.com/articles/getting-started-with-nodejs#set-up to set up the Heroku CLI on your machine.
     type: 'postgres',
-    // url: process.env.DATABASE_URL,
-    host: 'localhost',
-    port: 5432,
-    migrations: ['src/migration/*.js'],
+    url: process.env.DATABASE_URL,
+    migrations: ['src/migration_prod/*.js'],
     migrationsRun: true,
     entities: [User, Report],
     ssl: {
       rejectUnauthorized: false,
     },
-    username: 'root',
-    password: 'mehdi',
   } as DataSourceOptions,
 };
 
